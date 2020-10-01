d07ca8f3c0e399c2f415eeb1c0abd4b356f12082
mackzcon <mackzcon@gmail.com>
2020-10-01 22:03:33 +0530

    comments added for understading and clarity

--- scripts/logs.js
+++ scripts/logs.js
@@ -1,16 +1,20 @@
+// Constants defined for fs and path
 const fs = require('fs');
 const path = require('path');
 
 const logsPath = path.join(__dirname, '..', 'logs');
 
+// Create Directory function
 function createDir() {
   fs.mkdirSync(logsPath);
 }
 
+// Create file function
 function createFile(filename) {
   fs.writeFileSync(path.join(logsPath, `${filename}.log`), '');
 }
 
+// Define Main function - calling Create DIR and File
 function main() {
   if (fs.existsSync(logsPath)) {
     console.log('Logs already exists');
@@ -21,4 +25,5 @@ function main() {
   createFile('error');
 }
 
+// Calling the main function
 main();
