fd8dea74dd9fed884aa23a3a9c3a114342026b0c
Timothy Onyiuke <timothyonyiuke@gmail.com>
2020-12-12 11:52:27 +0100

    Revert "comments added for understading and clarity (#9)"
    This reverts commit e99a4d0a14579438207d7884880cac339b783be8.

--- scripts/logs.js
+++ scripts/logs.js
@@ -1,20 +1,16 @@
-// Constants defined for fs and path
 const fs = require('fs');
 const path = require('path');
 
 const logsPath = path.join(__dirname, '..', 'logs');
 
-// Create Directory function
 function createDir() {
   fs.mkdirSync(logsPath);
 }
 
-// Create file function
 function createFile(filename) {
   fs.writeFileSync(path.join(logsPath, `${filename}.log`), '');
 }
 
-// Define Main function - calling Create DIR and File
 function main() {
   if (fs.existsSync(logsPath)) {
     console.log('Logs already exists');
@@ -25,5 +21,4 @@ function main() {
   createFile('error');
 }
 
-// Calling the main function
 main();
